3810ff0cf31b9f757a4803d35b1c5a5f9e096638
Albert Lorinczi <88albertlorinczi@gmail.com>
2025-03-06 19:57:11 +0200

    added main

--- Seminar1_OOP.cpp
+++ Seminar1_OOP.cpp
@@ -1,6 +1,33 @@
 //
 // Created by 33alb on 3/6/2025.
 //
+
+#include <iostream>
+
+class Dog {
+  public:
+    bool eat() {
+      std::cout<<"Dog a mancat";
+      return true;
+    }
+  int setAge(int newAge) {
+      age = newAge;
+      return age;
+    }
+  int getAge() {
+      return age;
+    }
+  private:
+    int age;
+};
 int main(){
+  Dog dog;
+  dog.eat();
+  Dog dog1 = dog;
+  dog1.setAge(10);
+  dog1.getAge();
+  // dog1.age = 10;
+  // dog1.getAge();
+  std::cout<<dog1.getAge();
   return 0;
 }
